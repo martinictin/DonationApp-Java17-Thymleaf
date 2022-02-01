bb302d0f690140e310ce566bd6477f6d8b6f6b59
Prnjakis <80061949+Prnjakis@users.noreply.github.com>
2022-02-01 23:19:34 +0100

    userRegister added

--- src/main/java/com/donationapp/prototype/controller/UserController.java
+++ src/main/java/com/donationapp/prototype/controller/UserController.java
@@ -3,12 +3,15 @@ package com.donationapp.prototype.controller;
 import com.donationapp.prototype.model.User;
 import com.donationapp.prototype.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class UserController {
     @Autowired
     UserService userService;
@@ -16,7 +19,17 @@ public class UserController {
     @GetMapping("/allusers")
     public List<User> getAllUsers(){return userService.getAll();}
 
+    @GetMapping("/userRegister")
+    public String register(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
 
+    @PostMapping("/user")
+    public String submit(User user, Model model) {
+        model.addAttribute("user", user);
+        return "saved";
+    }
 
 
 }
