36b04d2ec97bdc0293b699cf310d14d5251467be
Prnjakis <80061949+Prnjakis@users.noreply.github.com>
2022-02-03 22:47:23 +0100

    main page edited,added registration for DonateArticle,working on displaying donations on index

--- src/main/java/com/donationapp/prototype/repository/UserRepository.java
+++ src/main/java/com/donationapp/prototype/repository/UserRepository.java
@@ -4,8 +4,9 @@ import com.donationapp.prototype.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
     User findByEmail(String email);
-    User findByUsername(String username);
-}
+    User findByUsername(String username);}
