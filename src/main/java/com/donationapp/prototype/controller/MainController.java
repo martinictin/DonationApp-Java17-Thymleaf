4ff23bcdef0cf068d2913c983814a254435bcf9e
Prnjakis <80061949+Prnjakis@users.noreply.github.com>
2022-02-03 23:10:19 +0100

    added methods to get data from database

--- src/main/java/com/donationapp/prototype/controller/MainController.java
+++ src/main/java/com/donationapp/prototype/controller/MainController.java
@@ -3,10 +3,14 @@ package com.donationapp.prototype.controller;
 import com.donationapp.prototype.model.User;
 import com.donationapp.prototype.repository.DonateArticleRepository;
 import com.donationapp.prototype.repository.UserRepository;
+import com.donationapp.prototype.service.DonateArticleService;
+import com.donationapp.prototype.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.sql.*;
 
 @Controller
 public class MainController {
@@ -15,10 +19,18 @@ public class MainController {
     UserRepository userRepository;
 
     @Autowired
+    UserService userService;
+
+    @Autowired
+    DonateArticleService donateArticleService;
+
+    @Autowired
     DonateArticleRepository donateArticleRepository;
 
     @GetMapping("/index")
-    public String viewMainPage(Model model) {
+    public String viewMainPage(Model model) throws ClassNotFoundException, SQLException {
+        userService.fromDbToRepo();
+        donateArticleService.fromDbToRepo();
         model.addAttribute("user",userRepository.findAll());
         model.addAttribute("donateArticle",donateArticleRepository.findAll());
         return "index";
