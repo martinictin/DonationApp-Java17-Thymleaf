e2f9d6201d0cefa3ede5088fcd773e8c035a549b
Prnjakis <80061949+Prnjakis@users.noreply.github.com>
2022-02-04 19:53:36 +0100

    Update UserController.java

--- src/main/java/com/donationapp/prototype/controller/UserController.java
+++ src/main/java/com/donationapp/prototype/controller/UserController.java
@@ -4,13 +4,14 @@ import com.donationapp.prototype.model.User;
 import com.donationapp.prototype.repository.UserRepository;
 import com.donationapp.prototype.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class UserController {
     @Autowired
     UserService userService;
